e3f8d85549048a9080f9c1c1c074409655ca59c2
PheeZz <jagubkoff@gmail.com>
2022-03-01 00:27:47 +0200

    begin of work with OpenCV

---
+++ btnFind.py
@@ -0,0 +1,61 @@
+import cv2
+import time
+import numpy as np
+import pyscreenshot as ImageGrab
+import pyautogui
+
+
+def find_patt(image, patt, thres):
+    img_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    (patt_H, patt_W) = patt.shape[:2]
+    res = cv2.matchTemplate(img_grey, patt, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res > thres)
+    return patt_H, patt_W, zip(*loc[::-1])
+
+
+def scanAndPress(button, flag=True):
+    screenshot = ImageGrab.grab()
+    img = np.array(screenshot.getdata(), dtype='uint8').reshape(
+        (screenshot.size[1], screenshot.size[0], 3))
+    patt = cv2.imread(button, 0)
+    h, w, points = find_patt(img, patt, 0.60)
+    if len(points) != 0:
+        pyautogui.moveTo(points[0][0]+w/2, points[0][1]+h/2)
+        pyautogui.click()
+        flag = False
+
+
+def chromeOpenScript():
+    flag = True
+    while flag:
+        time.sleep(0.5)
+        scanAndPress('buttons/cancelGoogle.png')
+        while flag:
+            # универсализация под светлую\темную тему
+            scanAndPress('buttons/useWebAppLight.png', flag)
+            scanAndPress('buttons/useWebAppDark.png', flag)
+            time.sleep(0.5)
+
+
+def edgeOpenScript():
+    flag = True
+    while flag:
+        time.sleep(0.5)
+        scanAndPress('buttons/cancelEdge.png')
+        while flag:
+            # универсализация под светлую\темную тему
+            scanAndPress('buttons/useWebAppLight.png', flag)
+            scanAndPress('buttons/useWebAppDark.png', flag)
+            time.sleep(0.5)
+
+
+def yandexOpenScript():
+    flag = True
+    while flag:
+        time.sleep(0.5)
+        scanAndPress('buttons/cancelYandex.png')
+        while flag:
+            # универсализация под светлую\темную тему
+            scanAndPress('buttons/useWebAppLight.png', flag)
+            scanAndPress('buttons/useWebAppDark.png', flag)
+            time.sleep(0.5)
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pyscreenshot as ps
+from btnFind import *
+
+import pyscreenshot as ImageGrab
+# Used to programmatically control the mouse & keyboard. (эмуляция мыши и клавиатуры)
+import pyautogui
 import numpy as np
 import cv2  # работа с изображениями
 
@@ -13,13 +17,30 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
 
 import datetime
+import time
 import os.path
 
 
-CLIENT_SECRET_FILE = 'client_secret.json'
-API_NAME = 'CALENDAR'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+def getDateTime(date):
+    iterator = 0
+    str_year, str_month, str_day, str_hours, str_minutes = "", "", "", "", ""
+    while iterator < 4:
+        str_year += date[iterator]
+        iterator += 1
+    iterator = 0
+    while iterator < 2:
+        str_month += date[5+iterator]
+        str_day += date[8+iterator]
+        str_hours += date[11+iterator]
+        str_minutes += date[14+iterator]
+        iterator += 1
+    year, month, day, hours, minutes = int(str_year), int(
+        str_month), int(str_day), int(str_hours), int(str_minutes)
+    return year, month, day, hours, minutes
+
+
+def timeNow(): return datetime.datetime.utcnow().isoformat()
+
 
 tmpPath = open('paths/chrome.txt')  # import app path from txt files
 googlePath = tmpPath.read()
@@ -57,7 +78,15 @@ calendar_id = currentCalendar['items'][0]['id']
 events_result = service.events().list(calendarId=calendar_id, timeMin=now,
                                       maxResults=1, singleEvents=True,
                                       orderBy='startTime').execute()
-events = events_result.get('items', [])
 
-currentClass = events_result['items'][0]['summary']
+currentClass = events_result['items'][0]['summary']  # геттер "названия" пары
+# геттер начала пары
 eventStartTime = events_result['items'][0]['start']['dateTime']
+link = events_result['items'][0]['location']  # геттер ссылки на пару
+arr = np.asarray(getDateTime(eventStartTime)
+                 )  # переводим тип turple в массив int'ов с помощью numpy для дальнейшей работы с datetime
+eventDateTimeDifference = datetime.datetime(
+    arr[0], arr[1], arr[2], arr[3], arr[4])
+
+arr = np.asarray(getDateTime(now))
+nowDifference = datetime.datetime(arr[0], arr[1], arr[2], arr[3], arr[4])
